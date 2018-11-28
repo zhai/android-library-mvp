f693ae037e9a67abf5c389272b8625627a6f3adc
zhai <masterzxg@gmail.com>
2018-11-28 15:20:28 +0800

    第一版本  借签于  google  todo-mvp 项目

---
+++ app/src/main/java/com/zhaisoft/lib/mvp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zhaisoft.lib.mvp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
